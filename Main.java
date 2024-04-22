a504a7c51ebe8ab1c36dfdc1f3c1c3832cd2fa00
Khalid Alharbi <kalharbi@users.noreply.github.com>
2024-04-22 21:45:35 +0300

    second commit

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
         int second = 20;
         // add two numbers
         int sum = first + second;
+        
         System.out.println(first + " + " + second + " = " + sum);
     }
 }
