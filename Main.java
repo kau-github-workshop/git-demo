8b60d46a40cbf20c2f56bf07eb2988cf7051c366
Khalid Alharbi <kalharbi@users.noreply.github.com>
2024-04-22 22:15:13 +0300

    updated this from my laptop

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ class Main {
     public static void main(String[] args) {
         System.out.println("..................");
         System.out.println("Adding two numbers");
+        System.out.println("..................");
+        System.out.println("Khalid");
         int x = 10;
         int y = 20;
         // add two numbers
